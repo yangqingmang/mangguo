fec39020a69306c6c0f26039669943a130ef8aca
yangqm <qmyang@g2link.cn>
2018-01-10 15:41:45 +0800

    增加新文件

---
+++ mangguo-service/src/main/java/com/yangqingmang/MangguoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yangqingmang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangguoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangguoServiceApplication.class, args);
+	}
+}
