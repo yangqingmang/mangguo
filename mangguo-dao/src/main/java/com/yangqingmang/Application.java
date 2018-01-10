ddd25a7a9d454346f51155a3d5e935b0d491ccbc
yangqm <qmyang@g2link.cn>
2018-01-10 20:20:01 +0800

    增加新文件

---
+++ mangguo-dao/src/main/java/com/yangqingmang/Application.java
@@ -0,0 +1,16 @@
+package com.yangqingmang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : yangqm
+ * @version :
+ * @date : Created in 下午7:28 2018/1/10
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
