4356a246b211cd32169ed52ad92780377d11e58f
yangqm <qmyang@g2link.cn>
2018-01-15 10:58:57 +0800

    增加新文件

---
+++ mangguo-test/src/main/java/com/yangqingmang/test/BaseTest.java
@@ -0,0 +1,16 @@
+package com.yangqingmang.test;
+
+import com.yangqingmang.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author : yangqm
+ * @version :
+ * @date : Created in 上午10:43 2018/1/15
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(properties = {},classes = Application.class)
+public class BaseTest {
+}
